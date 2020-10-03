d36870bdaf8447d45379018507eecac6eabd2c7a
Edwin Yu <theedwinyu888@gmail.com>
2020-10-03 12:03:30 -0400

    skeleton login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from './components/Login';
@@ -7,9 +6,11 @@ import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Login />
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/" component={Login} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import withFirebaseAuth from 'react-with-firebase-auth'
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConfig from '../firebaseConfig';
-import { Button } from 'antd';
+import { Button, Row, Col, Card } from 'antd';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAppAuth = firebaseApp.auth();
@@ -21,18 +21,32 @@ class Login extends Component {
         } = this.props;
 
         return (
-            <div>
-            {
-                user
-                  ? <p>Hello, {user.displayName}</p>
-                  : <p>Please sign in.</p>
-            }
+            <div className="App">
+                <header className="App-header" style={{ backgroundColor: '#4c29ff'}}>
 
-            {
-                user
-                ? <Button type="primary" onClick={signOut}>Sign out</Button>
-                : <Button type="primary" onClick={signInWithGoogle}>Sign in with Google!</Button>
-            }
+                    <Card bordered={false} style={{ backgroundColor: '#4c29ff'}}>
+                        <Row className="box-shadow" style={{ width:'100%', height:'100%' }}>
+                            <Col span={8} style={{ backgroundColor:'white', height: '100vh', color:'black' }}>
+                                <Card className="box-shadow login-centered">
+                                    <div>
+                                    {
+                                        user
+                                        ? <p>Hello, {user.displayName}</p>
+                                        : <p>Please sign in.</p>
+                                    }
+
+                                    {
+                                        user
+                                        ? <Button type="primary" onClick={signOut} style={{borderRadius: '10px'}}>Sign out</Button>
+                                        : <Button type="primary" onClick={signInWithGoogle} style={{borderRadius: '10px'}}>Sign in with Google!</Button>
+                                    }
+                                    </div>
+                                </Card>
+                            </Col>
+                            <Col span={16} className="login-right-background" />
+                        </Row>
+                    </Card>
+                </header>
             </div>
         );
     }
