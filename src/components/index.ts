ef3433bfcdba2576638279e0b08fd47d0d6b6e9f
SGinovker <sginovke@edu.uwaterloo.ca>
2020-10-03 21:55:51 -0400

    merge conflicts

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from './ListItem/ListItem';
 export * from './ListItemLink/ListItemLink';
 export * from './MasterDetail/MasterDetail';
 export * from './ProfileCard/ProfileCard';
+export * from './CourseContent/CourseContent';
